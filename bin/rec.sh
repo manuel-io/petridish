aaa8294bef4407177427e150a6903876a8447ecf
Manuel <m.a.n.u.e.l@posteo.net>
2017-07-15 11:07:09 +0200

    rec.sh

---
+++ bin/rec.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+i=0
+
+[ -z $1  ] &&
+echo "Usage: rec.sh url" && exit
+
+while true
+do
+  name=$(printf outout%0d $i)
+  avconv -i $1 -acodec libmp3lame -vcodec libx264 "${name}.avi"
+  echo $i
+  ((i++))
+done
