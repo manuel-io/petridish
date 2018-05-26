885da2b0978bc98e796e0095b8ec306dc205855b
Manuel <m.a.n.u.e.l@posteo.net>
2018-05-26 15:10:22 +0200

    Firefox 60.0.1

--- bin/useradd.sh
+++ bin/useradd.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
+
 SHELL=/usr/bin/zsh
 ERROR=/dev/stderr
-USER="username"
-ID=1200
+USER="worker"
+ID=1300
 
 groupadd -g $ID $USER
 useradd --uid $ID --gid $ID -s $SHELL -d /home/$USER $USER
 
-lvcreate -L 10G -n $USER users
+lvcreate -l 100%FREE -n $USER users
 mkfs.ext4 /dev/mapper/users-$USER
 mkdir /home/$USER
 mount /dev/mapper/users-$USER /home/$USER/
 chown -R $USER:$USER /home/$USER/
 
 echo "Add to your /etc/fstab:" > $ERROR
-echo "/dev/mapper/users-$USER /home/$USER ext4 defaults 0 2"
+echo "/dev/mapper/users-$USER /home/$USER ext4 defaults 0 2" > $ERROR
 
 passwd $USER
-#read
 
-#userdel $USER
-#groupdel $USER
-#umount /home/$USER/
-#lvremove -f users/$USER
-#rmdir /home/$USER/
+# userdel $USER
+# groupdel $USER
+# umount /home/$USER/
+# lvremove -f users/$USER
+# rmdir /home/$USER/
