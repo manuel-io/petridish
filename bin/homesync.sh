3b89695391bd8d270f865f19929871c7fe5261de
Manuel <m.a.n.u.e.l@posteo.net>
2018-10-11 13:47:49 +0200

    Quick fix

--- bin/homesync.sh
+++ bin/homesync.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-erro="/dev/stderr"
-exclude="repos movies mtp tmp"
+error="/dev/stderr"
+exclude="repos movies mtp tmp videos"
 exlist=""
 destdir=""
 
 [ -z $1 ] || [ ! -d $1 ] && {
-  echo "No destination directory" > $erro
+  echo "No destination directory" > $error
   exit 1
 }
 
@@ -20,5 +20,12 @@ done
 for dir in `find ${HOME}/ -mindepth 1 -maxdepth 1 -type d -name '[a-z]*'`
 do
   echo $dir
+
+  find $dir -type f -exec chmod a-x '{}' \;
+  find $dir -exec chmod go-rwx '{}' \;
+  find $dir -type f -exec chmod a-w '{}' \;
+
+  chown -R `id -un`:`id -gn` $dir
+
   rsync -rva --ignore-existing $exlist $dir $destdir
 done
