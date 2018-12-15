d15ad9d0a6a4e800b282ac3db6fc04d8b2d96349
Manuel <m.a.n.u.e.l@posteo.net>
2018-12-15 11:28:27 +0100

    Firefox 64

--- bin/homesync.sh
+++ bin/homesync.sh
@@ -10,6 +10,7 @@ destdir=""
   exit 1
 }
 
+cd $HOME
 destdir=$1
 
 for exfile in $exclude
@@ -21,11 +22,11 @@ for dir in `find ${HOME}/ -mindepth 1 -maxdepth 1 -type d -name '[a-z]*'`
 do
   echo $dir
 
-  find $dir -type f -exec chmod a-x '{}' \;
-  find $dir -exec chmod go-rwx '{}' \;
-  find $dir -type f -exec chmod a-w '{}' \;
+#  find $dir -type f -exec chmod a-x '{}' \;
+#  find $dir -exec chmod go-rwx '{}' \;
+#  find $dir -type f -exec chmod a-w '{}' \;
 
-  chown -R `id -un`:`id -gn` $dir
+#  chown -R `id -un`:`id -gn` $dir
 
   rsync -rva --ignore-existing $exlist $dir $destdir
 done
